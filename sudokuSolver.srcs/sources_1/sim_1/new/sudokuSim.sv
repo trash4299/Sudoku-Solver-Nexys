2f65419dc270e3521fdcd8aaf2430afb09823ef4
Vincent Lobo <vincelobo4299@gmail.com>
2019-08-29 13:53:52 -0400

    Serialized inputs and outputs

--- sudokuSolver.srcs/sources_1/sim_1/new/sudokuSim.sv
+++ sudokuSolver.srcs/sources_1/sim_1/new/sudokuSim.sv
@@ -1,46 +1,145 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/01/2019 11:40:58 AM
-// Design Name: 
-// Module Name: sudokuSim
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-`define CLOCKTIME 1000 //1000ns for 100MHz
+`define CLOCKTIME 100 //1000ns for 100MHz
 
  module sudokuSim();
-	reg  start_tb;
-	bit  clk_tb;
-	reg [323:0] enter_tb;
+	reg  start_tb = 0;
+	bit  clk_tb = 0;
+	//reg[323:0] enter_tb;
+	//wire[323:0] outputs_tb;
+	reg[323:0] enter_reg;
+	reg[3:0] enter_tb;
 	wire readyToStart_tb;
+	wire busy_tb;
 	wire done_tb;
-	wire [323:0] out_tb;
+	wire[3:0] outputs_tb;
+	
+	integer yeet = 0, counting = 0;
 	
 	always #`CLOCKTIME clk_tb <= ~clk_tb;
 	
-	sudoku_v DUT(.start(start_tb),.clk(clk_tb),.enter(enter_tb),.readyToStart(readyToStart_tb),.done(done_tb),.out(out_tb));
+	sudoku_v DUT(.start(start_tb),.clk(clk_tb),.enter(enter_tb),.readyToStart(readyToStart_tb),.done(done_tb),.outputs(outputs_tb));
 	
 	initial begin
-		clk_tb = 0;
-		enter_tb <= {{4'd0,4'd7,4'd6,4'd3,4'd1,4'd4,4'd9,4'd5,4'd8},{4'd8,4'd5,4'd4,4'd9,4'd6,4'd2,4'd7,4'd1,4'd3},{4'd9,4'd1,4'd3,4'd8,4'd7,4'd5,4'd2,4'd6,4'd4},{4'd4,4'd6,4'd8,4'd1,4'd2,4'd7,4'd3,4'd9,4'd5},{4'd5,4'd9,4'd7,4'd4,4'd3,4'd8,4'd6,4'd2,4'd1},{4'd1,4'd3,4'd2,4'd5,4'd9,4'd6,4'd4,4'd8,4'd7},{4'd3,4'd2,4'd5,4'd7,4'd8,4'd9,4'd1,4'd4,4'd6},{4'd6,4'd4,4'd1,4'd2,4'd5,4'd3,4'd8,4'd7,4'd9},{4'd7,4'd8,4'd9,4'd6,4'd4,4'd1,4'd5,4'd3,4'd2}};
-		#10ns;
-		start_tb <= 1;
-		#15ns;
-		//start_tb <= 0;
-		#20000ns;
-		$finish;
+		enter_reg <= {{4'd0,4'd7,4'd6,4'd3,4'd1,4'd4,4'd9,4'd5,4'd8},{4'd8,4'd5,4'd4,4'd9,4'd6,4'd2,4'd7,4'd1,4'd3},{4'd9,4'd1,4'd3,4'd8,4'd7,4'd5,4'd2,4'd6,4'd4},{4'd4,4'd6,4'd8,4'd1,4'd2,4'd7,4'd3,4'd9,4'd5},{4'd5,4'd9,4'd7,4'd4,4'd3,4'd8,4'd6,4'd2,4'd0},{4'd1,4'd3,4'd2,4'd5,4'd9,4'd6,4'd4,4'd8,4'd7},{4'd3,4'd2,4'd5,4'd7,4'd8,4'd9,4'd1,4'd4,4'd6},{4'd6,4'd4,4'd1,4'd2,4'd5,4'd3,4'd8,4'd7,4'd9},{4'd7,4'd8,4'd9,4'd6,4'd4,4'd1,4'd5,4'd3,4'd2}};
+		
+		// #10ns;
+		// start_tb <= 1;
+		// #100ns;
+		// start_tb <= 0;
+		// #20000ns;
+		// $finish;
 	end
+	
+	always@(posedge clk_tb) begin
+		case(yeet)
+			0 : begin
+				yeet <= yeet +1;
+			end
+			1 : begin
+				
+				yeet <= yeet +1;
+			end
+			2 : begin
+				yeet <= yeet +1;
+			end
+			3 : begin
+				start_tb <= 1;
+				yeet <= yeet +1;
+			end
+			4 : begin
+				start_tb <= 0;
+				enter_tb <= enter_reg[(323-counting*4)-:4];
+				counting <= counting + 1;
+				if(counting == 81) begin
+					counting <= 0;
+					yeet <= yeet +1;
+				end
+			end
+			4 : begin
+				if(done_tb )
+					yeet <= yeet +1;
+			end
+			5 : begin
+				counting <= counting + 1;
+				if(counting == 81)
+					yeet <= yeet +1;
+			end
+			6 : begin
+				yeet <= yeet +1;
+			end
+			7 : begin
+				
+				yeet <= yeet +1;
+			end
+			8 : begin
+				
+				yeet <= yeet +1;
+			end
+			9 : begin
+				
+				yeet <= yeet +1;
+			end
+			10 : begin
+				
+				yeet <= yeet +1;
+			end
+			11 : begin
+				
+				yeet <= yeet +1;
+			end
+			12 : begin
+				
+				yeet <= yeet +1;
+			end
+			13 : begin
+				
+				yeet <= yeet +1;
+			end
+			14 : begin
+				
+				yeet <= yeet +1;
+			end
+			15 : begin
+				
+				yeet <= yeet +1;
+			end
+			16 : begin
+				
+				yeet <= yeet +1;
+			end
+			17 : begin
+				
+				yeet <= yeet +1;
+			end
+			18 : begin
+				
+				yeet <= yeet +1;
+			end
+			19 : begin
+				
+				yeet <= yeet +1;
+			end
+			20 : begin
+				
+				yeet <= yeet +1;
+			end
+			21 : begin
+				
+				yeet <= yeet +1;
+			end
+			22 : begin
+				
+				yeet <= yeet +1;
+			end
+			23 : begin
+				
+				yeet <= yeet +1;
+			end
+			24 : begin
+				
+				yeet <= yeet +1;
+			end
+		endcase
+	end
+	
 endmodule
